e26ab2e45ca7fdc0c462cdd94f06865a91adb90d
Shahbaz <jocnud@gmail.com>
2017-01-08 22:04:40 +0530

    added centralised clsConfigServer

--- clsMessageService/src/main/java/com/cloudsea/messages/ClsMessageServiceApplication.java
+++ clsMessageService/src/main/java/com/cloudsea/messages/ClsMessageServiceApplication.java
@@ -1,9 +1,13 @@
 package com.cloudsea.messages;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 
@@ -19,4 +23,18 @@ public class ClsMessageServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ClsMessageServiceApplication.class, args);
 	}
+}
+
+@RestController
+@RefreshScope
+class TestControler {
+
+	@Value("${message}")
+	private String messageHello;
+
+	@RequestMapping("/hello")
+	public String getProertyMessage() {
+		return messageHello;
+	}
+
 }
