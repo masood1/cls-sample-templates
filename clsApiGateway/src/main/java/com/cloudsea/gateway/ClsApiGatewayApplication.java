a71622fee4a77f9fb6a03816e410a828931b4ea1
Shahbaz <jocnud@gmail.com>
2017-01-09 02:51:34 +0530

    Add Zull Micro Proxy Implementaion as irst step to make API gateway

---
+++ clsApiGateway/src/main/java/com/cloudsea/gateway/ClsApiGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.cloudsea.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ClsApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClsApiGatewayApplication.class, args);
+	}
+}
